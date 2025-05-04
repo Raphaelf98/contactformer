683c980880eff2c565a225aec00e0d1f94eda1fd
Raphael <r.ullrich.1@campus.tu-berlin.de>
2025-05-04 22:26:56 +0200

    fps ablation

--- scripts/ptv2trainroboeval.sh
+++ scripts/ptv2trainroboeval.sh
@@ -3,7 +3,7 @@
 # Set the CUDA deviceexport 
 CUDA_DEVICE_ORDER=PCI_BUS_ID
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=4
 # Define the checkpoint directory
 CKPT_DIR="checkpoints/ptv2-revised-robo-eval"
 OVERWRITE_CKPT_DIR=1
